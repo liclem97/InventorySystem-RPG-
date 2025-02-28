b8fbc7047050b8e08929d768a11e335a3ea2943a
WonJeong <liclem@naver.com>
2025-02-28 18:02:00 +0900

    Create a data table for items

--- Source/RPGInventorySystem/Public/RPGInventorySystem.h
+++ Source/RPGInventorySystem/Public/RPGInventorySystem.h
@@ -3,3 +3,31 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "RPGInventorySystem.generated.h"
+
+USTRUCT(BlueprintType)
+struct FItemStruct : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	FText Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	FText Description;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	int32 StackSize;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	UTexture2D* Image;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	float Power;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	UStaticMesh* Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	USkeletalMesh* SkeletalMesh;
+};
